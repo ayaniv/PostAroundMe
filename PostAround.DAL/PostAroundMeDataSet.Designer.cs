80d689f3a7529804827599653b981a5022e66c81
Yaniv Aharon <ayaniv@gmail.com>
2015-07-09 18:10:12 +0300

    Making New laptop working

--- PostAround.DAL/PostAroundMeDataSet.Designer.cs
+++ PostAround.DAL/PostAroundMeDataSet.Designer.cs
@@ -271,7 +271,6 @@ SELECT ID, Latitude, Longitude, CategoryID, UserID, Title, Description, ImageUrl
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Data.Design.TypedDataSetGenerator", "4.0.0.0")]
         private void InitConnection() {
             this._connection = new global::System.Data.SqlClient.SqlConnection();
-            this._connection.ConnectionString = global::PostAround.DAL.Properties.Settings.Default.PostAroundConnectionString;
         }
         
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
